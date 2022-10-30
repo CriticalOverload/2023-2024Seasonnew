62af122ff93038597d7cf1f22eaee14a2844258c
criti <critical.overlord@gmail.com>
2022-10-30 18:03:42 -0400

    10/30 - Added the claw code and attempted to fix joystick controls for the wheels.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -3,11 +3,16 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
 
 @TeleOp(name = "Main TeleOp")
 public class MainTeleOp extends LinearOpMode {
     private DcMotor motorFR,motorBR,motorFL,motorBL, motorLS;
     private RobotClass2 robot;
+    private Servo servo;
+    double servoPosition = 1.0;
     @Override
     public void runOpMode() throws InterruptedException{
         motorFR = hardwareMap.dcMotor.get("FR");
@@ -15,6 +20,9 @@ public class MainTeleOp extends LinearOpMode {
         motorFL = hardwareMap.dcMotor.get("FL");
         motorBL = hardwareMap.dcMotor.get("BL");
         motorLS = hardwareMap.dcMotor.get("LS");
+        servo = hardwareMap.servo.get("claw");
+        servo.setPosition(servoPosition);
+
 
         robot = new RobotClass2(motorFL, motorFR, motorBL, motorBR, motorLS, this);
         robot.setupRobot();
@@ -32,14 +40,24 @@ public class MainTeleOp extends LinearOpMode {
             //right+left-
             //up-down+?!<<<<<<<<<<
             //drive
-            double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) + 3*(Math.PI/4);//-(0)-45=45
+//            double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) + 3*(Math.PI/4);//-(0)-45=45
+//            double r = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
+//            double rotation = gamepad1.left_stick_x;
+//
+//            double leftPower = r*Math.sin(angle);//1
+//            double rightPower = r*Math.cos(angle);//-1
+
+            double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) + (Math.PI/4);
             double r = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
             double rotation = gamepad1.left_stick_x;
 
-            double leftPower = r*Math.sin(angle);//1
-            double rightPower = r*Math.cos(angle);//-1
+            double powerOne = r*Math.sin(angle);
+            double powerTwo = r*Math.cos(angle);
 
-
+            motorFL.setPower((powerOne - (rotation))*powerMod);
+            motorFR.setPower((powerTwo + (rotation))*powerMod);
+            motorBL.setPower((powerTwo - (rotation))*powerMod);
+            motorBR.setPower((powerOne + (rotation))*powerMod);
 
             double circumference = 4.409;//circumference of pulley for hub
             double groundRN = 2/circumference; // rotations needed to reach point from 0
@@ -47,32 +65,41 @@ public class MainTeleOp extends LinearOpMode {
             double medRN =26/circumference;
             double highEP =37/circumference;// (inches from ground / circumference)* ticks
 
-            motorFL.setPower((leftPower - (rotation))*powerMod);
-//            motorFR.setPower((rightPower + (rotation))*powerMod);
-            motorLS.setPower((rightPower + (rotation))*powerMod);
-            motorBL.setPower((rightPower - (rotation))*powerMod);
-            motorBR.setPower((leftPower + (rotation))*powerMod);
 
+//            motorFL.setPower((leftPower - (rotation))*powerMod);
+//            motorFR.setPower((rightPower + (rotation))*powerMod);
+//            motorBL.setPower((rightPower - (rotation))*powerMod);
+//            motorBR.setPower((leftPower + (rotation))*powerMod);
+            motorLS.setPower(-gamepad2.right_stick_y * 0.5);
+
+
+            //Claw
+            if (gamepad2.dpad_up) {
+                servo.setPosition(0.5);
+            }
+            else if (gamepad2.dpad_down) {
+                servo.setPosition(1.0);
+            }
 
             //Linear Slide
             if (gamepad2.a) {
                 motorLS.getCurrentPosition();
-                motorLS.setTargetPosition(1);
+                motorLS.setTargetPosition((int)groundRN);
                 motorLS.setPower(.5); /*figure out what value is best for this*/
             }
             else if (gamepad2.x) {
                 motorLS.getCurrentPosition();
-                motorLS.setTargetPosition(2);
+                motorLS.setTargetPosition((int)smallRN);
                 motorLS.setPower(.5); /*figure out what value is best for this*/
             }
             else if (gamepad2.b) {
                 motorLS.getCurrentPosition();
-                motorLS.setTargetPosition(3);
+                motorLS.setTargetPosition((int)medRN);
                 motorLS.setPower(.5); /*figure out what value is best for this*/
             }
             else if (gamepad2.y) {
                 motorLS.getCurrentPosition();
-                motorLS.setTargetPosition(4);
+                motorLS.setTargetPosition((int)highEP);
                 motorLS.setPower(.5); /*figure out what value is best for this*/
             }
             else {
